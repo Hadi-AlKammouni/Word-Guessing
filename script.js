1f30369cf90a125812a26cdfc35551e10cc9dd79
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-10-08 16:46:13 +0300

    js/ Getting random object from the wordList upon each refresh

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Guess a word!")
+function randomWord() {
+    // getting random object from the wordList list
+    let ran_obj = wordList[Math.floor(Math.random() * wordList.length)]
+    let word = ran_obj.word
+}
+
+randomWord()
