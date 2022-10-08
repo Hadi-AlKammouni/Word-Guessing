83a8d5322931805ad7182fb115c1af5e032ddbc0
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-10-08 16:50:32 +0300

    js/ Creating number of empty boxes equal to the word length

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+const inputs = document.querySelector(".inputs")
+
 function randomWord() {
     // getting random object from the wordList list
     let ran_obj = wordList[Math.floor(Math.random() * wordList.length)]
     let word = ran_obj.word
+
+    // creating number of empty boxes equal to the word length
+    let html = ""
+    for(let i=0; i < word.length; i++) {
+        html += '<input type="text" disabled>'
+    }
+    inputs.innerHTML = html
 }
 
 randomWord()
