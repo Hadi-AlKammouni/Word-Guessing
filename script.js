02abc67e3643617cdd8e7503ca79aca5d264b8be
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-10-08 17:14:33 +0300

    js/ Receiving the input entered by the user

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const inputs = document.querySelector(".inputs"),
-reset_btn = document.querySelector(".reset-btn")
+reset_btn = document.querySelector(".reset-btn"),
+hint = document.querySelector(".hint span"),
+typing_input = document.querySelector(".typing-input")
 
 function randomWord() {
     // getting random object from the wordList list
     let ran_obj = wordList[Math.floor(Math.random() * wordList.length)]
     let word = ran_obj.word
+
+    hint.innerText = ran_obj.hint
 
     // creating number of empty boxes equal to the word length
     let html = ""
@@ -16,4 +20,11 @@ function randomWord() {
 
 randomWord()
 
+function initGame(e) {
+    let key = e.target.value
+    console.log(key)
+}
+
 reset_btn.addEventListener("click", randomWord)
+typing_input.addEventListener("input", initGame)
+document.addEventListener("keydown", () => typing_input.focus())
